bf93cf158af1cbac9c934ffa7d014e4d95fc210b
dabit3 <dabit3@gmail.com>
2021-06-11 17:41:41 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Image from 'next/image'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <div
+          className="pt-4 pb-2 px-10 border-b"
+        >
+          <Image src="/icon.svg" height={50} width={50} />
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
